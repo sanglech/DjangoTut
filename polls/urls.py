1905146e9e5ec3bfb43c3d486091c8d5f75f3c23
Christian Sangle <christian.sangle@mail.utoronto.ca>
2020-05-01 16:23:28 +0900

    Refactored and added test cases (stepb 5)

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name='polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/ - Look at question 5
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/ results for question 5
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/ vote for question 5
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
